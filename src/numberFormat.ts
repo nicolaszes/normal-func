e786154e03e1958b3e86e9293d22eb85f9d69fa8
nicolaszs <1316236724@qq.com>
2019-07-08 15:12:29 +0800

    add float reservered

--- src/numberFormat.ts
+++ src/numberFormat.ts
@@ -1,4 +1,43 @@
-export const thousandSesymbol = (val: number): string => {
-  const numberREG = /(?<=\b(?<!\.)\d*)\B(?=(\d{3})+(?=\b))/g
+type SesymbolType = number | null | undefined
+
+/**
+ * 数字千分符号格式化
+ * @param {Number} val
+ */
+export const thousandSesymbol = (val: SesymbolType): string => {
+  if (typeof val !== 'number') {
+    throw new Error('val must be number')
+  }
+
+  const numberREG: RegExp = /(?<=\b(?<!\.)\d*)\B(?=(\d{3})+(?=\b))/g
   return val.toString().replace(numberREG, ',')
+}
+
+type ReservedType = number | null | undefined
+
+/**
+ * 小数点后保留几位有效数字
+ * @param {Number} val
+ * @param {Number} target
+ * 是否四舍五入
+ * @param {Boolean} ceil
+ */
+export const floatReserverd = (
+  val: ReservedType,
+  target: number = 2,
+  ceil: Boolean = false
+) => {
+  if (typeof val !== 'number') {
+    throw new Error('val must be number')
+  }
+
+  if (ceil) {
+    return Number(val.toFixed(target))
+  }
+
+  let flag = val > 0 ? 1 : -1
+  return (
+    (flag * Math.floor(flag * val * Math.pow(10, target))) /
+    Math.pow(10, target)
+  )
 }
--- test/numberFormat.spec.ts
+++ test/numberFormat.spec.ts
@@ -1,7 +1,11 @@
-import { thousandSesymbol } from '../src/numberFormat'
+import { thousandSesymbol, floatReserverd } from '../src/numberFormat'
 
 // thousand sesymbol test
 describe('thousandSesymbol match number', () => {
+  it('throw error', () => {
+    expect(() => thousandSesymbol(undefined || null)).toThrow()
+  })
+
   it('number less than 1', () => {
     expect(thousandSesymbol(0.123456)).toBe('0.123456')
   })
@@ -16,5 +20,36 @@ describe('thousandSesymbol match number', () => {
 
   it('minus number', () => {
     expect(thousandSesymbol(-123456.78901)).toBe('-123,456.78901')
+  })
+})
+
+// thousand sesymbol test
+describe('floatReserverd match number', () => {
+  it('throw error', () => {
+    expect(() => floatReserverd(undefined || null)).toThrow()
+  })
+
+  it('number less than 1', () => {
+    expect(floatReserverd(0.123456)).toBe(0.12)
+  })
+
+  it('number morn than 1', () => {
+    expect(floatReserverd(1.23456)).toBe(1.23)
+  })
+
+  it('number morn than 1000', () => {
+    expect(floatReserverd(123456.789)).toBe(123456.78)
+  })
+
+  it('minus number', () => {
+    expect(floatReserverd(-123456.78901)).toBe(-123456.78)
+  })
+
+  it('add target', () => {
+    expect(floatReserverd(0.123456, 3)).toBe(0.123)
+  })
+
+  it('add ceil', () => {
+    expect(floatReserverd(0.123456, 3, true)).toBe(0.123)
   })
 })
